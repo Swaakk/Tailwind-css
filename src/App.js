8a8293acb13ebde703f6aa82c08c3a62c7e9cbd8
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-23 22:02:32 +0530

    finished header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
       <div>
         <h1 className='font-bold text-lg text-right mr-10  text-green-400'></h1>
       </div>
