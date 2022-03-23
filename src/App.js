103381a8b684ab7fe2c29ba0fec4f598d9ceea25
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-23 22:00:24 +0530

    started project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        <h1 className='font-bold text-lg text-right mr-10  text-green-400'></h1>
+      </div>
+
+      
     </div>
   );
 }
